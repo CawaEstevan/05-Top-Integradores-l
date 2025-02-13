38599b1627f0081b016eba131cba0b0c8bc56b91
CawaEstevan <145603770+CawaEstevan@users.noreply.github.com>
2025-02-13 23:01:03 +0000

    a

--- aula00-JavaScript/aula00_ex2.js
+++ aula00-JavaScript/aula00_ex2.js
@@ -14,3 +14,23 @@ const soma = function(a,b){
 }
 
 console.log(soma("Rony","Rústico"));
+
+// Arrow Function (função de seta) =>
+
+const multiplicacao = (x,y) => x * y;
+console.log(multiplicacao(5,6));
+
+//Objeto em javascript
+
+const  Pessoa = {
+    nomeUsuario:"Rony bike",
+    idade:"29",
+    email:"padastro das maria",
+    usuario:"Atacante bikezera",
+    saudar: function(){
+        return "oi, meu nome é " + this.nomeUsuario +" e eu sou um" + this.usuario + ".";
+    }
+
+} 
+console.log(Pessoa.nomeUsuario);
+console.log(Pessoa.saudar());
